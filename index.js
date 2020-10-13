2b12ca0cd77950e5023f28635f07cf114ad7dcb0
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-13 09:21:33 -0500

    adds encryption

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
-    const input = document.querySelector('input')
+    const input = document.querySelector('#message-input')
+    const encrypted = btoa(input.value)
+    const linkInput = document.querySelector('#link-input')
+    linkInput.value = encrypted
+        
+
 })
