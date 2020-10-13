606ffe64e22a90b40454a01b4b702edb29d44256
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-13 09:41:12 -0500

    adds message display conditionally

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const { hash } = window.location;
+
+const message = atob(hash.replcace('#', ''))
+
+if (message) {
+    document.querySelector('#message-form').classList.add('hide')
+    const showMessage = document.querySelector('#show-message')
+    showMessage.querySelector('h1').textContent = message;
+    showMessage.classList.remove('hide')
+}
+
+
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     const input = document.querySelector('#message-input')
